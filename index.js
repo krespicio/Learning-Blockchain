2229b859ed078eb3cffce36e1ac87da7e687c3eb
Kyle Respicio <kyle.b.respicio@gmail.com>
2019-07-21 19:02:57 -0400

    Added a transaction class along with ability to see a miner's currency level

--- index.js
+++ index.js
@@ -3,10 +3,18 @@
 
 const sha256 = require("crypto-js/sha256");
 
+class Transaction {
+    constructor(from, to, amount) {
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+    }
+}
+
 class Block {
-    constructor(data, previous) {
+    constructor(transaction, previous) {
         this.timeStamp = new Date();
-        this.data = data;
+        this.transaction = transaction;
         this.previousHash = previous;
         this.hash = this.calculateHash();
         this.nonce = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
@@ -14,7 +22,7 @@ class Block {
 
     calculateHash() {
         return sha256(
-            this.timeStamp + this.previousHash + JSON.stringify(this.data) + this.nonce
+            this.timeStamp + this.previousHash + JSON.stringify(this.transaction) + this.nonce
         ).toString();
     }
 
@@ -23,15 +31,15 @@ class Block {
             this.nonce = this.nonce + 1;
             this.hash = this.calculateHash();
         }
-
-        console.log("MineCraft: " + this.hash);
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
-        this.difficulty = 4;
+        this.difficulty = 1;
+        this.pendingTransactions = [];
+        this.miningReward = 5;
     }
 
     createGenesisBlock() {
@@ -42,10 +50,38 @@ class Blockchain {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(data) {
-        let newBlock = new Block(data, this.getLatestBlock().hash);
+    // addBlock(data) {
+    //     let newBlock = new Block(data, this.getLatestBlock().hash);
+    //     newBlock.mineBlock(this.difficulty);
+    //     this.chain.push(newBlock);
+    // }
+
+    createTransaction(transaction) {
+        this.pendingTransactions.push(transaction);
+    }
+
+    // This is structured if each block can only contain a single transaction
+    minePendingTransactions(minerAddress) {
+        if (this.pendingTransactions.length === 0) {
+            throw "There are currently no transactions";
+        }
+        let newBlock = new Block(this.pendingTransactions.pop(), this.getLatestBlock().hash);
         newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
+
+        this.pendingTransactions = [new Transaction(null, minerAddress, this.miningReward)];
+    }
+
+    checkMinerBalance(minerAddress) {
+        return this.chain.reduce((acc, block) => {
+            if (minerAddress === block.transaction.from) {
+                return acc - block.transaction.amount;
+            } else if (minerAddress === block.transaction.to) {
+                return acc + block.transaction.amount;
+            } else {
+                return acc + 0;
+            }
+        }, 0);
     }
 
     validate() {
@@ -68,19 +104,35 @@ class Blockchain {
 }
 
 let coinChain = new Blockchain();
-coinChain.addBlock({ to: "Sherman", from: "Kyle", payment: "15" });
-coinChain.addBlock({ to: "Sherman", from: "Memo", payment: "25" });
+const uberEatsTransaction = new Transaction("Kyle", "Uber", 20);
+const uberTransaction = new Transaction("Kyle", "Uber", 43);
+coinChain.createTransaction(uberEatsTransaction);
+coinChain.createTransaction(uberTransaction);
+coinChain.minePendingTransactions("Sherman");
+coinChain.minePendingTransactions("Sherman");
+console.log("Kyle has a balance of", coinChain.checkMinerBalance("Kyle"));
+console.log("Uber has a balance of", coinChain.checkMinerBalance("Uber"));
+console.log("Sherman has a balance of", coinChain.checkMinerBalance("Sherman"));
 
-console.log(JSON.stringify(coinChain, null, 4));
-console.log("Is this chain valid?", coinChain.validate() ? "yes" : "no");
+////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////Additional Tesing////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
 
-console.log("Modify the blockchain: Sherman hacks the payment");
-coinChain.chain[1].data = { to: "Sherman", from: "Kyle", payment: "100" };
+// coinChain.addBlock({ to: "Sherman", from: "Kyle", payment: "15" });
+// coinChain.addBlock({ to: "Sherman", from: "Memo", payment: "25" });
+
 // console.log(JSON.stringify(coinChain, null, 4));
-console.log("Is this chain valid?", coinChain.validate() ? "yes" : "no");
+// console.log("Is this chain valid?", coinChain.validate() ? "yes" : "no");
 
-console.log("Modify the blockchain: Sherman hacks the payment and recalculates the hash");
-coinChain.chain[1].data = { to: "Sherman", from: "Kyle", payment: "100" };
-coinChain.chain[1].hash = coinChain.chain[1].calculateHash();
-// console.log(JSON.stringify(coinChain, null, 4));
-console.log("Is this chain valid?", coinChain.validate() ? "yes" : "no");
+// console.log("Modify the blockchain: Sherman hacks the payment");
+// coinChain.chain[1].data = { to: "Sherman", from: "Kyle", payment: "100" };
+// // console.log(JSON.stringify(coinChain, null, 4));
+// console.log("Is this chain valid?", coinChain.validate() ? "yes" : "no");
+
+// console.log("Modify the blockchain: Sherman hacks the payment and recalculates the hash");
+// coinChain.chain[1].data = { to: "Sherman", from: "Kyle", payment: "100" };
+// coinChain.chain[1].hash = coinChain.chain[1].calculateHash();
+// // console.log(JSON.stringify(coinChain, null, 4));
+// console.log("Is this chain valid?", coinChain.validate() ? "yes" : "no");
+
+// console.log(new Transaction("sherman", "kyle", 50));
